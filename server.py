72ca75f66ec150bfdb21dc842fd26366e379551e
sarad <saradekjae@gmail.com>
2025-09-17 13:06:08 +0200

    data fra arduino til python

---
+++ server.py
@@ -0,0 +1,17 @@
+import serial
+import time
+
+def readarduino():
+    while True:
+        data = ser.readline().decode().strip()
+        if "stationarytarget" in data or "movingtarget" in data:
+            return true
+        elif "notarget" in data
+            return false
+
+while true:
+    status = readarduino
+
+    time.sleep(1)
+
+if __name__ == '__main__':

d660a9f32b1e9c93113b56be419342b6e43cd69e
Fredehjort <160583406+Fredehjort@users.noreply.github.com>
2025-09-19 08:46:27 +0200

    fikset server.py's kan ikke få data fra sensor fejl.
    Fandt problemet. nu kan vi faktisk printe data og modtage data.

--- server.py
+++ server.py
@@ -7,14 +7,17 @@ ser = serial.Serial('COM4', 115200, timeout=1)
 def readarduino():
     """Reads a line from Arduino and returns True if target detected, False otherwise"""
     while True:
-        if ser.in_waiting > 0:  # only read if data is available
-            data = ser.readline().decode(errors='ignore').strip().lower()
-            if "stationary target" in data or "moving target" in data:
+        if ser.in_waiting > 0: # only read if data is available
+            data = ser.readline().decode(errors='ignore').strip()
+            print("RAW:", data)  # vi har nu et debug print så vi kan tjekke om vi faktisk får data. hjælper med at se om programmet kører ordenligt ved at printe den "rå" data.
+            if "Stationary target" in data or "Moving target" in data: # fandt fejlen, den leder efter noget som var for specifkt, så den skal bare lede efter keywords
                 print("1 or more targets have been found (target detected)")
-                return True
+                print("Raw data:", data) # printer den data vi får fra sensoren her.
+        return True
             elif "no target" in data:
                 print("no target")
                 return False
+
 
 if __name__ == '__main__':
     while True:
